e1fe7d2f718bd31d384a48d140021c8a7dd15696
moneerrahmani <73264229+moneerrahmani@users.noreply.github.com>
2020-11-04 21:36:05 -0500

    Update TestClass.java
    I can see the changes in eclips

--- src/test/java/testNG/concepts/TestClass.java
+++ src/test/java/testNG/concepts/TestClass.java
@@ -7,6 +7,9 @@ public class TestClass {
 	@Test 
 	public void test() {
 		System.out.println("This class will push to repo");
+		
+		// this is the practice 
+		// I should see the changes in the e clips
 	}
 
 }
